d26bc1d1ecf91bebe21d121b863d367ea94274ff
TedDay5 <144728376+TedDay5@users.noreply.github.com>
2024-09-23 18:45:17 -0600

    First commit

--- code.js
+++ code.js
@@ -1,3 +1,17 @@
 function binarySearch(list, element) {
+    let right = list.length - 1;
+    let left = 0;
+
+    while (right >= left) {
+        let middle = ((left + right)/2);
+
+        if (list[middle] === element) {
+            return middle;
+        } else if (list[middle] < element) {
+            left = middle + 1;
+        } else{
+            right = middle - 1;
+        }
+    }
     return -1;
 }
