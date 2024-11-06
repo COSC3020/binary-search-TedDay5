7d78d88026be60307aa6bc09f1c41e7abd8e5a7b
TedDay5 <144728376+TedDay5@users.noreply.github.com>
2024-11-05 21:28:53 -0700

    Added in cases where there's no middle

--- code.js
+++ code.js
@@ -1,17 +1,29 @@
 function binarySearch(list, element) {
     let right = list.length - 1;
     let left = 0;
+    let middle = math.round(left + (right - left)/2)
 
-    while (left <= right) {
-        let middle = Math.floor((left + right)/2);
-
-        if (list[middle] === element) {
-            return middle;
-        } else if (list[middle] < element) {
-            left = middle + 1;
-        } else{
-            right = middle - 1;
+    if (list[middle] == element) {
+        let index = middle
+        while (left <= right) {
+            if (list[middle] == element) {
+                index = middle;
+                right = middle - 1;
+            } else if (element < list[middle]) {
+                right = middle - 1;
+            } else{
+                left = middle + 1;
+            }
         }
+        return index
+    }
+    else if (list[middle] > element) {
+        let mindex = binarySearch(list.slice(0, middle), element);
+        return mindex;
+    }
+    else if (list[middle] < element) {
+        let mindex = binarySearch(list.slice(0, right + 1), element);
+        return mindex + middle;
     }
     return -1;
 }
